62a0ffbe7dba92cbaaff69e86a337d4142358aaa
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-01-12 23:38:26 +0530

    complete expres part 1

--- Express JS/Part 1/index.js
+++ Express JS/Part 1/index.js
@@ -25,19 +25,37 @@ app.get("/", (req, res) => {
     res.send("root path");
 })
 
-app.get("/home", (req, res) => {
-    res.send("home path");
-})
-app.get("/about", (req, res) => {
-    res.send("about path");
-});
+// app.get("/home", (req, res) => {
+//     res.send("home path");
+// })
+// app.get("/about", (req, res) => {
+//     res.send("about path");
+// });
 
-app.get("*", (req, res) => {
-    res.send("This path is not exist");
-});
-app.post("/", (req, res) => {
-    res.send("You send a post req");
-});
+// app.get("/Hello", (req, res) => {
+//     res.send("Hello path");
+// });
+// app.get("*", (req, res) => {
+//     res.send("This path is not exist");
+// });
+// app.post("/", (req, res) => {
+//     res.send("You send a post req");
+// });
+
+// app.get("/:username/:id", (req, res) => {
+//     let { username, id } = req.params;
+//     res.send(`<h1 font="arial">Welcome to page of @${username}!</h1>`);
+// });
+
+
+app.get("/search", (request, respose) => {
+    // console.log(request.query);
+    let { q } = request.query;
+    if (!q) {
+        respose.send("<h1>404 not found </h1>")
+    }
+    respose.send(`Search result ${q}`)
+})
 
 
 
