308449f7d8ddc59249fd5493ee39cfd25873ff01
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-01-13 23:35:08 +0530

    complete EJS

--- Node EJS/index.js
+++ Node EJS/index.js
@@ -5,6 +5,9 @@ const path = require('path');
 
 let port = 8080;
 
+app.use(express.static(path.join(__dirname, "/public/css")));
+app.use(express.static(path.join(__dirname, "/public/js")));
+
 app.listen(port, () => {
     console.log(`listening on port : ${port}`);
 });
@@ -26,6 +29,11 @@ app.set("views", path.join(__dirname, "/views"))
 // Passing data to EJS //
 // =================== //
 
+app.get("/", (req, res) => {
+    let diceValue = Math.floor(Math.random() * 6) + 1;
+    res.render("rollDice.ejs", { diceValue });
+})
+
 app.get("/rollDice", (req, res) => {
     let diceValue = Math.floor(Math.random() * 6) + 1;
     res.render("rollDice.ejs", { diceValue })
@@ -37,10 +45,14 @@ app.get("/rollDice", (req, res) => {
 // ============= //
 // create a basic template for insagram pages based on following Route.
 
-app.get("/ig/:username", (req, res) => {
+app.get("/:username", (req, res) => {
     let { username } = req.params;
-    const instaData = require('./data.json');
+    const instaData = require("./data.json");
     let data = instaData[username];
-    console.log("Data : " + data);
-    res.render("instagram.ejs", { data })
+    console.log(username);
+    if (data) {
+        res.render("instagram.ejs", { data });
+    } else {
+        res.render("error.ejs")
+    }
 })
