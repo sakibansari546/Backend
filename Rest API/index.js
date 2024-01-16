4468978ff72f1a3a55312361c93f161dba20055b
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-01-16 23:59:42 +0530

    aa

--- Rest API/index.js
+++ Rest API/index.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const port = 8080;
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
+
+app.use(methodOverride('_method'));
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -67,7 +70,7 @@ app.post("/posts", (req, res) => {
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
     let post = posts.find((post) => id == post.id);
-    console.log(post);
+    // console.log(post);
     res.render("show.ejs", { post })
 })
 
@@ -85,8 +88,31 @@ app.get("/posts/:id", (req, res) => {
 // ========================== //
 // Update Route
 
-// app.patch('/posts/:id', (req, res) => {
-//     let { id } = req.params;
-//     console.log(id);
-//     res.send("Patch woking")
-// })
+app.patch('/posts/:id', (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let newUsername = req.body.username;
+
+    let post = posts.find((post) => id == post.id);
+
+    post.content = newContent;
+    post.username = newUsername;
+
+    console.log(post);
+    res.redirect("/posts");
+})
+
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+
+    let post = posts.find((post) => id == post.id);
+
+    res.render("edit.ejs", { post });
+})
+
+
+// ================== //
+// Implement : Delete //
+// ================== //
+Destroy Route
